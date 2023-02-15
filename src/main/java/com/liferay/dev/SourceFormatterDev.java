eda06f9a2f3c7fc332bd109e6a8a2a5f8af055fb
Kevin Lee <kevin.lee@liferay.com>
2023-02-15 08:58:05 -0800

    Use SourceFormatter.java instead

--- src/main/java/com/liferay/dev/SourceFormatterDev.java
+++ src/main/java/com/liferay/dev/SourceFormatterDev.java
@@ -1,26 +1,15 @@
 package com.liferay.dev;
 
-import com.liferay.petra.string.CharPool;
-import com.liferay.petra.string.StringPool;
-import com.liferay.portal.kernel.util.ArrayUtil;
-import com.liferay.portal.kernel.util.ListUtil;
-import com.liferay.portal.kernel.util.StringUtil;
-import com.liferay.portal.tools.ArgumentsUtil;
 import com.liferay.portal.tools.GitException;
-import com.liferay.portal.tools.GitUtil;
-import com.liferay.portal.tools.ToolsUtil;
 import com.liferay.source.formatter.SourceFormatter;
 import com.liferay.source.formatter.SourceFormatterArgs;
 
 import com.puppycrawl.tools.checkstyle.api.CheckstyleException;
 
-import java.util.Arrays;
-import java.util.Map;
-
 public class SourceFormatterDev {
 
 	public static void main(String[] args) throws Exception {
-		SourceFormatterArgs sourceFormatterArgs = _getSourceFormatterArgs(args);
+		SourceFormatterArgs sourceFormatterArgs = _getSourceFormatterArgs(true);
 
 		try {
 			SourceFormatter sourceFormatter = new SourceFormatter(
@@ -66,172 +55,39 @@ public class SourceFormatterDev {
 		}
 	}
 
-	private static SourceFormatterArgs _getSourceFormatterArgs(String[] args)
-		throws Exception {
-
-		Map<String, String> arguments = ArgumentsUtil.parseArguments(args);
+	private static SourceFormatterArgs _getSourceFormatterArgs(boolean debug) {
 		SourceFormatterArgs sourceFormatterArgs = new SourceFormatterArgs();
 
-		sourceFormatterArgs.setAutoFix(
-			ArgumentsUtil.getBoolean(
-				arguments, "source.auto.fix", SourceFormatterArgs.AUTO_FIX));
-
-		String baseDirName = ArgumentsUtil.getString(
-			arguments, "source.base.dir", SourceFormatterArgs.BASE_DIR_NAME);
-
-		sourceFormatterArgs.setBaseDirName(baseDirName);
-
-		sourceFormatterArgs.setCheckCategoryNames(
-			ListUtil.fromString(
-				ArgumentsUtil.getString(
-					arguments, "source.check.category.names", null),
-				StringPool.COMMA));
-		sourceFormatterArgs.setCheckNames(
-			ListUtil.fromString(
-				ArgumentsUtil.getString(arguments, "source.check.names", null),
-				StringPool.COMMA));
+		sourceFormatterArgs.setAutoFix(SourceFormatterArgs.AUTO_FIX);
+		sourceFormatterArgs.setBaseDirName(SourceFormatterArgs.BASE_DIR_NAME);
 		sourceFormatterArgs.setFailOnAutoFix(
-			ArgumentsUtil.getBoolean(
-				arguments, "source.fail.on.auto.fix",
-				SourceFormatterArgs.FAIL_ON_AUTO_FIX));
+			SourceFormatterArgs.FAIL_ON_AUTO_FIX);
 		sourceFormatterArgs.setFailOnHasWarning(
-			ArgumentsUtil.getBoolean(
-				arguments, "source.fail.on.has.warning",
-				SourceFormatterArgs.FAIL_ON_HAS_WARNING));
+			SourceFormatterArgs.FAIL_ON_HAS_WARNING);
 		sourceFormatterArgs.setFormatCurrentBranch(
-			ArgumentsUtil.getBoolean(
-				arguments, "format.current.branch",
-				SourceFormatterArgs.FORMAT_CURRENT_BRANCH));
+			SourceFormatterArgs.FORMAT_CURRENT_BRANCH);
 		sourceFormatterArgs.setFormatLatestAuthor(
-			ArgumentsUtil.getBoolean(
-				arguments, "format.latest.author",
-				SourceFormatterArgs.FORMAT_LATEST_AUTHOR));
+			SourceFormatterArgs.FORMAT_LATEST_AUTHOR);
 		sourceFormatterArgs.setFormatLocalChanges(
-			ArgumentsUtil.getBoolean(
-				arguments, "format.local.changes",
-				SourceFormatterArgs.FORMAT_LOCAL_CHANGES));
+			SourceFormatterArgs.FORMAT_LOCAL_CHANGES);
 		sourceFormatterArgs.setGitWorkingBranchName(
-			ArgumentsUtil.getString(
-				arguments, "git.working.branch.name",
-				SourceFormatterArgs.GIT_WORKING_BRANCH_NAME));
-
-		int commitCount = ArgumentsUtil.getInteger(
-			arguments, "commit.count", SourceFormatterArgs.COMMIT_COUNT);
-
-		sourceFormatterArgs.setCommitCount(commitCount);
-
-		if (commitCount > 0) {
-			sourceFormatterArgs.addRecentChangesFileNames(
-				GitUtil.getModifiedFileNames(baseDirName, commitCount),
-				baseDirName);
-		}
-		else if (sourceFormatterArgs.isFormatCurrentBranch()) {
-			sourceFormatterArgs.addRecentChangesFileNames(
-				GitUtil.getCurrentBranchFileNames(
-					baseDirName, sourceFormatterArgs.getGitWorkingBranchName(),
-					false),
-				baseDirName);
-		}
-		else if (sourceFormatterArgs.isFormatLatestAuthor()) {
-			sourceFormatterArgs.addRecentChangesFileNames(
-				GitUtil.getLatestAuthorFileNames(baseDirName, false),
-				baseDirName);
-		}
-		else if (sourceFormatterArgs.isFormatLocalChanges()) {
-			sourceFormatterArgs.addRecentChangesFileNames(
-				GitUtil.getLocalChangesFileNames(baseDirName, false),
-				baseDirName);
-		}
-
-		String[] fileNames = StringUtil.split(
-			ArgumentsUtil.getString(
-				arguments, "source.files", StringPool.BLANK),
-			StringPool.COMMA);
-
-		if (ArrayUtil.isNotEmpty(fileNames)) {
-			sourceFormatterArgs.setFileNames(Arrays.asList(fileNames));
-		}
-		else {
-			String fileExtensionsString = ArgumentsUtil.getString(
-				arguments, "source.file.extensions", StringPool.BLANK);
-
-			String[] fileExtensions = StringUtil.split(
-				fileExtensionsString, StringPool.COMMA);
-
-			sourceFormatterArgs.setFileExtensions(
-				Arrays.asList(fileExtensions));
-		}
-
+			SourceFormatterArgs.GIT_WORKING_BRANCH_NAME);
+		sourceFormatterArgs.setCommitCount(SourceFormatterArgs.COMMIT_COUNT);
 		sourceFormatterArgs.setIncludeGeneratedFiles(
-			ArgumentsUtil.getBoolean(
-				arguments, "include.generated.files",
-				SourceFormatterArgs.INCLUDE_GENERATED_FILES));
-
-		boolean includeSubrepositories = ArgumentsUtil.getBoolean(
-			arguments, "include.subrepositories",
+			SourceFormatterArgs.INCLUDE_GENERATED_FILES);
+		sourceFormatterArgs.setIncludeSubrepositories(
 			SourceFormatterArgs.INCLUDE_SUBREPOSITORIES);
-
-		for (String recentChangesFileName :
-				sourceFormatterArgs.getRecentChangesFileNames()) {
-
-			if (recentChangesFileName.endsWith("ci-merge")) {
-				includeSubrepositories = true;
-
-				break;
-			}
-		}
-
-		sourceFormatterArgs.setIncludeSubrepositories(includeSubrepositories);
-
 		sourceFormatterArgs.setMaxLineLength(
-			ArgumentsUtil.getInteger(
-				arguments, "max.line.length",
-				SourceFormatterArgs.MAX_LINE_LENGTH));
-		sourceFormatterArgs.setMaxDirLevel(
-			Math.max(
-				ToolsUtil.PORTAL_MAX_DIR_LEVEL,
-				StringUtil.count(baseDirName, CharPool.SLASH) + 1));
+			SourceFormatterArgs.MAX_LINE_LENGTH);
+		sourceFormatterArgs.setMaxDirLevel(SourceFormatterArgs.MAX_DIR_LEVEL);
 		sourceFormatterArgs.setOutputFileName(
-			ArgumentsUtil.getString(
-				arguments, "output.file.name",
-				SourceFormatterArgs.OUTPUT_FILE_NAME));
-		sourceFormatterArgs.setPrintErrors(
-			ArgumentsUtil.getBoolean(
-				arguments, "source.print.errors",
-				SourceFormatterArgs.PRINT_ERRORS));
+			SourceFormatterArgs.OUTPUT_FILE_NAME);
+		sourceFormatterArgs.setPrintErrors(SourceFormatterArgs.PRINT_ERRORS);
 		sourceFormatterArgs.setProcessorThreadCount(
-			ArgumentsUtil.getInteger(
-				arguments, "processor.thread.count",
-				SourceFormatterArgs.PROCESSOR_THREAD_COUNT));
-		sourceFormatterArgs.setShowDebugInformation(
-			ArgumentsUtil.getBoolean(
-				arguments, "show.debug.information",
-				SourceFormatterArgs.SHOW_DEBUG_INFORMATION));
-
-		String[] skipCheckNames = StringUtil.split(
-			ArgumentsUtil.getString(
-				arguments, "skip.check.names", StringPool.BLANK),
-			StringPool.COMMA);
-
-		if (ArrayUtil.isNotEmpty(skipCheckNames)) {
-			sourceFormatterArgs.setSkipCheckNames(
-				Arrays.asList(skipCheckNames));
-		}
-
-		String[] sourceFormatterProperties = StringUtil.split(
-			ArgumentsUtil.getString(
-				arguments, "source.formatter.properties", StringPool.BLANK),
-			"\\n");
-
-		if (ArrayUtil.isNotEmpty(sourceFormatterProperties)) {
-			sourceFormatterArgs.setSourceFormatterProperties(
-				Arrays.asList(sourceFormatterProperties));
-		}
-
+			SourceFormatterArgs.PROCESSOR_THREAD_COUNT);
+		sourceFormatterArgs.setShowDebugInformation(debug);
 		sourceFormatterArgs.setValidateCommitMessages(
-			ArgumentsUtil.getBoolean(
-				arguments, "validate.commit.messages",
-				SourceFormatterArgs.VALIDATE_COMMIT_MESSAGES));
+			SourceFormatterArgs.VALIDATE_COMMIT_MESSAGES);
 
 		return sourceFormatterArgs;
 	}
