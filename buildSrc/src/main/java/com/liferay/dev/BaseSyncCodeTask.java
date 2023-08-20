bb848c257dbaa366d26750186c5110b81631fd6f
Kevin Lee <kevin.lee@liferay.com>
2023-08-20 13:57:29 -0700

    Simplify sync code tasks

--- buildSrc/src/main/java/com/liferay/dev/BaseSyncCodeTask.java
+++ buildSrc/src/main/java/com/liferay/dev/BaseSyncCodeTask.java
@@ -1,47 +1,73 @@
 package com.liferay.dev;
 
 import com.github.fracpete.rsync4j.RSync;
-import org.gradle.api.DefaultTask;
-import org.gradle.api.Project;
-import org.gradle.api.tasks.TaskAction;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+
+import org.gradle.api.DefaultTask;
+import org.gradle.api.Project;
+import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.TaskAction;
 
 /**
  * @author Kevin Lee
  */
 public abstract class BaseSyncCodeTask extends DefaultTask {
 
-    @TaskAction
-    public final void sync() throws Exception {
-        Project project = getProject();
+	@Input
+	@Override
+	public String getGroup() {
+		return "dev";
+	}
 
-        Path repoPath = Paths.get(
-                String.valueOf(project.findProperty("liferay.portal.repo.dir")));
+	@TaskAction
+	public final void syncCode() throws Exception {
+		Project project = getProject();
 
-        Path sourcePath = getSourcePath(repoPath);
-        Path destinationPath = getDestinationPath(repoPath);
+		if (!project.hasProperty("liferay.portal.repo.dir")) {
+			throw new Exception(
+				"Please provide path to your Liferay Portal repository by " +
+					"setting 'liferay.portal.repo.dir' in 'gradle.properties'");
+		}
 
-        if (Files.notExists(destinationPath)) {
-            Files.createDirectories(destinationPath);
-        }
+		Path repoPath = Paths.get(
+			String.valueOf(project.property("liferay.portal.repo.dir")));
 
-        RSync rsync = new RSync()
-                .source(sourcePath.toString())
-                .destination(destinationPath.toString())
-                .exclude("**/com/liferay/dev")
-                .recursive(true)
-                .delete(true)
-                .deleteExcluded(false)
-                .quiet(true);
+		Path sourcePath = getSourcePath(repoPath);
 
-        rsync.execute();
-    }
+		if (Files.notExists(sourcePath)) {
+			throw new Exception(sourcePath + " does not exist");
+		}
 
-    protected abstract Path getSourcePath(Path repoPath);
+		Path destinationPath = getDestinationPath(repoPath);
 
-    protected abstract Path getDestinationPath(Path repoPath);
+		if (Files.notExists(destinationPath)) {
+			Files.createDirectories(destinationPath);
+		}
+
+		RSync rsync = new RSync();
+
+		rsync.source(
+			sourcePath.toString()
+		).destination(
+			destinationPath.toString()
+		).exclude(
+			"**/com/liferay/dev"
+		).recursive(
+			true
+		).delete(
+			true
+		).deleteExcluded(
+			false
+		).quiet(
+			true
+		).execute();
+	}
+
+	protected abstract Path getDestinationPath(Path repoPath);
+
+	protected abstract Path getSourcePath(Path repoPath);
 
 }
--- buildSrc/src/main/java/com/liferay/dev/PushSyncCodeTask.java
+++ buildSrc/src/main/java/com/liferay/dev/PushSyncCodeTask.java
@@ -3,19 +3,28 @@ package com.liferay.dev;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.gradle.api.tasks.Input;
+
 /**
  * @author Kevin Lee
  */
 public class PushSyncCodeTask extends BaseSyncCodeTask {
 
-    @Override
-    protected Path getSourcePath(Path repoPath) {
-        return Paths.get("./src/main");
-    }
+	@Input
+	@Override
+	public String getDescription() {
+		return "Syncs code from this project to your `liferay-portal` " +
+			"repository";
+	}
 
-    @Override
-    protected Path getDestinationPath(Path repoPath) {
-        return repoPath.resolve("modules/util/source-formatter/src");
-    }
+	@Override
+	protected Path getDestinationPath(Path repoPath) {
+		return repoPath.resolve("modules/util/source-formatter/src");
+	}
+
+	@Override
+	protected Path getSourcePath(Path repoPath) {
+		return Paths.get("./src/main");
+	}
 
 }
